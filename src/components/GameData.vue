b15deac552769c2df865cc905316cbcd8cdedd39
SpBills <spencebills0@gmail.com>
2021-11-07 05:11:56 +0000

    Added click to copy and join page link.

--- src/components/GameData.vue
+++ src/components/GameData.vue
@@ -1,21 +1,34 @@
 <template>
-	<div>
+	<div class="flex flex-col">
 		<p>Game Code: {{ gameId }}</p>
+		<a class="mt-3 text-blue-400 cursor-pointer" @click="onClick">Click to copy a link</a>
+        <small class="text-xs" v-if="copied">Copied to clipboard</small>
 	</div>
 </template>
 
 <script>
 import QrcodeVue from "qrcode.vue";
+import { useRouter } from 'vue-router';
+import { ref } from '@vue/reactivity';
 
 export default {
-    components: {
-        QrcodeVue
-    },
-    props: {
-        gameId: {
-            type: String,
-            required: true
-        }
-    }
-}
+	components: {
+		QrcodeVue,
+	},
+	props: {
+		gameId: {
+			type: String,
+			required: true,
+		},
+	},
+	setup({gameId}) {
+        const copied = ref(false);
+		const onClick = () => {
+			navigator.clipboard.writeText(location.protocol + `//` + location.host + `/join/${gameId}`);
+            copied.value = true;
+		};
+
+		return { onClick, copied };
+	},
+};
 </script>
--- src/views/JoinPage.vue
+++ src/views/JoinPage.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<div></div>
+		<h1>{{error}}</h1>
 	</div>
 </template>
 
@@ -13,12 +13,20 @@ import {
 	signInWithPopup,
 	User,
 } from "firebase/auth";
-import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
+import { arrayUnion, collection, doc, DocumentData, getDoc, getDocs, getFirestore, setDoc, updateDoc } from "firebase/firestore";
+import { useRoute, useRouter } from 'vue-router';
+import Game from '@/models/Game';
+import EnrolledUser from '@/models/EnrolledUser';
 export default {
 	setup() {
+		const route = useRoute();
+		const router = useRouter();
+
 		const auth = getAuth();
 		const db = getFirestore();
+		const error = ref("");
 		const user = ref({} as User);
+		const games: DocumentData[] = [];
 		const login = async () => {
 			const provider = new GoogleAuthProvider();
 			await signInWithPopup(auth, provider);
@@ -28,25 +36,61 @@ export default {
 		};
 
 		const getUser = async () => {
-			onAuthStateChanged(auth, async (u) => {
+			onAuthStateChanged(auth, (u) => {
 				if (u) {
-                    user.value = u!;
-					const email = user.value.email!;
+					user.value = u!;
 
-					const userRef = doc(db, "users", email);
-					const docSnap = await getDoc(userRef);
-                    user.value = docSnap.data() as User;
 				} else {
 					login();
 				}
 			});
 		};
 
-		getUser();
+		const join = async () => {
+			const id = route.params.id.toString();
+			const game = games.find((game) => game.id == id)!;
+			const gameData = game.data() as Game;
+			const gameRef = doc(db, "games", id);
+			const userRef = doc(db, "users", user.value.email!);
 
-		const join = () => {};
+			const userObj = {
+				name: user.value.displayName,
+				email: user.value.email,
+				sortId: Math.floor(Math.random() * 100000),
+				eliminated: false
+			} as EnrolledUser;
 
-		join();
+			if (!gameData.won) {
+				await updateDoc(gameRef, {
+					users: arrayUnion(userObj),
+				});
+
+				await updateDoc(userRef, {
+					game: game.id,
+				});
+				router.push("/")
+			} else {
+				error.value = "Game has already finished.";
+			}
+		};
+
+		const onLoad = async () => {
+			const querySnapshot = await getDocs(collection(db, "games"));
+			querySnapshot.forEach((doc) => {
+				games.push(doc);
+			});
+
+			await getUser();
+			join();
+		};
+
+		onLoad();
+
+
+		return {
+			user,
+			error
+		}
 	},
 };
 </script>
