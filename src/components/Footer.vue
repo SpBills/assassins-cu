6b1de71869dfa49f743b46f35ca6ad64d7eaeb8b
SpBills <spencebills0@gmail.com>
2021-11-18 19:42:06 +0000

    Added footer.

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="flex">
+		<p>Made by Spencer Bills</p>
+		<a class="ml-auto text-blue-400" href="bitcoincash:qz8k8y59juyt4k8594v4rf9cp5uzmgrctcjklzq502"
+			>
+            Consider Donating
+			<small class="text-xs">(as low as $0.01!)</small>
+            </a
+		>
+	</div>
+</template>
--- src/views/JoinGame.vue
+++ src/views/JoinGame.vue
@@ -78,6 +78,7 @@ export default {
 			if (gameData.users.find((user) => user.email === userObj.email) !== undefined) {
 				// Do something later.
 				// TODO
+				router.replace("/");
 			}
 
 			if (!gameData.won) {
